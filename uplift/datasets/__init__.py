8a6bf2bad8c2bc69e22ed77c793818491a3a5497
Paulius Šarka <paulius.sarka@gmail.com>
2016-06-19 19:31:33 +0200

    radcliffe surry example generator

---
+++ uplift/datasets/__init__.py
@@ -0,0 +1,3 @@
+from .samples_generator import make_radcliffe_surry
+
+__all__ = ['make_radcliffe_surry']
---
+++ uplift/datasets/samples_generator.py
@@ -0,0 +1,22 @@
+from ..utils import check_random_state
+
+
+def make_radcliffe_surry(n_samples=64000, p_target=0.5,  a_positive=0.5, target_base_uplift=0.03,
+                         target_b_uplift=0.1, return_uplift=False, random_state=None):
+
+    generator = check_random_state(random_state)
+    nrm = a_positive + target_base_uplift + target_b_uplift
+
+    X = generator.rand(n_samples, 2)
+    group = generator.binomial(1, p_target, size=n_samples)
+
+    p_control = X[:,0] * a_positive / nrm
+    uplift = (X[:, 1]*target_b_uplift + target_base_uplift) / nrm
+
+    y = generator.binomial(1, p_control + uplift * group)
+
+    if return_uplift:
+        return X, y, group, uplift
+    else:
+        return X, y, group
+
